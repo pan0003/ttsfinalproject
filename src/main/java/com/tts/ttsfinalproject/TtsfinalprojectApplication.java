ce45b08d64ef028b3e653ba2916765806bc8c2ee
pan0003 <75246763+pan0003@users.noreply.github.com>
2021-01-27 10:08:40 -0600

    first commit

---
+++ src/main/java/com/tts/ttsfinalproject/TtsfinalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.tts.ttsfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtsfinalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtsfinalprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/ttsfinalproject/TtsfinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.ttsfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtsfinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
